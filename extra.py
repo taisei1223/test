212dcd34de6f2d36b574a2280ea6346f25db809c
taisei1223 <163708756+taisei1223@users.noreply.github.com>
2024-10-19 05:58:54 +0000

    test

--- extra.py
+++ extra.py
@@ -1,4 +1,7 @@
+import pandas as pd
+from pyodide.http import open_url
+
 def main():
     url = "https://raw.githubusercontent.com/taisei1223/create_game/main/onepice_titiles.csv"
-    df = pd.read_csv(open_url(url), header=None)
-    return df
+    result = pd.read_csv(open_url(url), header=None)
+    return result
